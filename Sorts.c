2fd4c5a367ede934803289cffea34f92a69b7361
prakash-iitp <110123947+prakash-iitp@users.noreply.github.com>
2022-07-28 01:07:06 +0530

    added insertion sort algorithm

--- Sorts.c
+++ Sorts.c
@@ -27,3 +27,19 @@ void Bubble(int n,int a[])
 
             }
 }
+
+void insert(int a[], int n) /* function to sort an aay with insertion sort */  
+{  
+    int i, j, temp;  
+    for (i = 1; i < n; i++) {  
+        temp = a[i];  
+        j = i - 1;  
+  
+        while(j>=0 && temp <= a[j])  /* Move the elements greater than temp to one position ahead from their current position*/  
+        {    
+            a[j+1] = a[j];     
+            j = j-1;    
+        }    
+        a[j+1] = temp;    
+    }  
+}
