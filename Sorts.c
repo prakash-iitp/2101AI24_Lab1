63b7006eae98f5a112c01d7fdeabab23e2c0790b
prakash-iitp <110123947+prakash-iitp@users.noreply.github.com>
2022-07-28 01:05:49 +0530

    added algorithm for bubble sort

--- Sorts.c
+++ Sorts.c
@@ -9,3 +9,21 @@ int swap(int *n1, int *n2)
 
     return 0;
 }
+
+//Function Definiton for Bubble Sort
+void Bubble(int n,int a[])
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+
+        for (j = 0; j < n - i - 1; j++)
+
+            if (a[j] > a[j + 1])
+            {
+                //Algorithm for swapping two numbers
+                a[j]=a[j] - a[j+1];
+                a[j + 1]=a[j] + a[j + 1];
+                a[j]=a[j+1] - a[j];
+
+            }
+}
