cacf17bf0a23a54016c6df8e979ee0b43627171f
Prakash Kumar <prakash_2101ai24@iitp.ac.in>
2022-07-27 23:36:57 +0530

    User choosed operation

--- calculator.c
+++ calculator.c
@@ -6,4 +6,21 @@ int main()
     int n1, n2;
     float res;
     printf (" Choose an operator(+, -, *, /) to perform the operation in C Calculator \n ");
-    scanf ("%c", &opt); // take an operator 
+    scanf ("%c", &opt); // take an operator
+    if (operator == '/' )
+        {
+            printf (" You have selected: Division");
+        }
+        else if (operator == '*')
+        {
+            printf (" You have selected: Multiplication");
+         }
+
+        else if (operator == '-')
+        {
+            printf (" You have selected: Subtraction");
+         }
+            else if (operator == '+')
+        {
+            printf (" You have selected: Addition");
+         }
