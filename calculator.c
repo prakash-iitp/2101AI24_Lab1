a65af1c8b6435738bb52ce17653ea99b698b4030
Prakash Kumar <prakash_2101ai24@iitp.ac.in>
2022-07-27 23:33:25 +0530

    Declared variable and asked fo choosing operator

---
+++ calculator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    // declare local variables
+    char opt;
+    int n1, n2;
+    float res;
+    printf (" Choose an operator(+, -, *, /) to perform the operation in C Calculator \n ");
+    scanf ("%c", &opt); // take an operator 
